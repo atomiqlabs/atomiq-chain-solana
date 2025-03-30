c62295e05f6c6afe5676afd9d8ac192c124f3340
adambor <adamborteam@gmail.com>
2025-03-31 01:13:15 +0200

    Fix blockTime in event meta

--- dist/solana/events/SolanaChainEventsBrowser.js
+++ dist/solana/events/SolanaChainEventsBrowser.js
@@ -127,6 +127,7 @@ class SolanaChainEventsBrowser {
                     break;
             }
             parsedEvent.meta = {
+                blockTime: eventObject.blockTime,
                 timestamp: eventObject.blockTime,
                 txId: eventObject.signature
             };
