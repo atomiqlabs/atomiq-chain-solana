915adeaa3fe2d911f33334bb60c1ebf3c80152b2
adambor <adamborteam@gmail.com>
2026-08-05 11:57:24 +0200

    Make getInitAuthorizationExpiry explictly check block timestamp instead of local time

--- dist/solana/swaps/modules/SwapInit.js
+++ dist/solana/swaps/modules/SwapInit.js
@@ -438,9 +438,14 @@ class SwapInit extends SolanaSwapModule_1.SolanaSwapModule {
             const slotsLeft = lastValidTransactionSlot - latestSlot + this.SIGNATURE_SLOT_BUFFER;
             if (slotsLeft < 0)
                 return true;
+            const latestBlock = await this.root.Blocks.getParsedBlock(latestSlot);
+            if ((parseInt(timeout) + this.program._authGracePeriod) < latestBlock.blockTime)
+                return true;
         }
-        if ((parseInt(timeout) + this.program._authGracePeriod) * 1000 < Date.now())
-            return true;
+        else {
+            if ((parseInt(timeout) + this.program._authGracePeriod) * 1000 < Date.now())
+                return true;
+        }
         return false;
     }
     /**
